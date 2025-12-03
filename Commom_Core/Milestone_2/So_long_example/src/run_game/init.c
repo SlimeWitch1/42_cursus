569ed33f301aea0787df290badb01a1173ec059c
SlimeWitch1 <tukaemrys@gmail.com>
2025-12-02 22:28:40 -0300

    Fix variable name typo in init_game_structs

--- Commom_Core/Milestone_2/So_long_example/src/run_game/init.c
+++ Commom_Core/Milestone_2/So_long_example/src/run_game/init.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   init.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: turocha- <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/22 21:57:43 by turocha-          #+#    #+#             */
-/*   Updated: 2025/11/22 21:57:45 by turocha-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../game.h"
 
 void	load_images(t_game *game)
@@ -34,25 +22,25 @@ void	load_images(t_game *game)
 			&game->tile_width, &game->tile_height);
 }
 
-t_game	*init_game_structs(int **matrix, int *row_and_collumn)
+t_game	*init_game_structs(int **matrix, int *row_and_column)
 {
 	t_game	*game;
 
 	game = malloc(sizeof(t_game));
 	if (!game)
-		malloc_error(NULL, matrix, row_and_collumn);
+		malloc_error(NULL, matrix, row_and_column);
 	game->map = NULL;
 	game->player = NULL;
 	game->mlx = NULL;
 	game->window = NULL;
 	game->map = malloc(sizeof(t_map));
 	if (!game->map)
-		malloc_error(game, matrix, row_and_collumn);
+		malloc_error(game, matrix, row_and_column);
 	game->player = malloc(sizeof(t_player));
 	if (!game->player)
-		malloc_error(game, matrix, row_and_collumn);
+		malloc_error(game, matrix, row_and_column);
 	game->map->matrix = matrix;
-	game->map->row_and_collumn = row_and_collumn;
+	game->map->row_and_column = row_and_column;
 	game->end = 0;
 	game->can_exit = 0;
 	return (game);
