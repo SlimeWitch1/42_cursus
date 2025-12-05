92b31fe90dd1d84ad5070cbbb070c57196b32cf6
SlimeWitch1 <tukaemrys@gmail.com>
2025-12-05 16:54:12 -0300

    Implement game initialization and image loading
    This file initializes the game structures and loads images for the game elements.

---
+++ Commom_Core/Milestone_2/So_long/src/run_game/init.c
@@ -0,0 +1,48 @@
+
+#include "../game.h"
+
+void	load_images(t_game *game)
+{
+	game->wall_img = NULL;
+	game->floor_img = NULL;
+	game->player->img = NULL;
+	game->exit_closed_img = NULL;
+	game->exit_open_img = NULL;
+	game->collect_img = NULL;
+	game->floor_img = mlx_xpm_file_to_image(game->mlx, "textures/floor.xpm",
+			&game->tile_width, &game->tile_height);
+	game->wall_img = mlx_xpm_file_to_image(game->mlx, "textures/wall.xpm",
+			&game->tile_width, &game->tile_height);
+	game->collect_img = mlx_xpm_file_to_image(game->mlx, "textures/collect.xpm",
+			&game->coll_width, &game->coll_height);
+	game->player->img = mlx_xpm_file_to_image(game->mlx, "textures/player.xpm",
+			&game->player->width, &game->player->height);
+	game->exit_closed_img = mlx_xpm_file_to_image(game->mlx,
+			"textures/exit_closed.xpm", &game->tile_width, &game->tile_height);
+	game->exit_open_img = mlx_xpm_file_to_image(game->mlx, "textures/exit_open.xpm",
+			&game->tile_width, &game->tile_height);
+}
+
+t_game	*init_game_structs(int **matrix, int *row_and_column)
+{
+	t_game	*game;
+
+	game = malloc(sizeof(t_game));
+	if (!game)
+		malloc_error(NULL, matrix, row_and_column);
+	game->map = NULL;
+	game->player = NULL;
+	game->mlx = NULL;
+	game->window = NULL;
+	game->map = malloc(sizeof(t_map));
+	if (!game->map)
+		malloc_error(game, matrix, row_and_column);
+	game->player = malloc(sizeof(t_player));
+	if (!game->player)
+		malloc_error(game, matrix, row_and_column);
+	game->map->matrix = matrix;
+	game->map->row_and_column = row_and_column;
+	game->end = 0;
+	game->can_exit = 0;
+	return (game);
+}
