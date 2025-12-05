b0924c570e5ba237a3570c7b714a000b130ce1bb
SlimeWitch1 <tukaemrys@gmail.com>
2025-12-05 16:54:41 -0300

    Refactor memory management in free.c
    Updated memory management functions to handle errors and corrected variable names for consistency.

--- Commom_Core/Milestone_2/So_long/src/free.c
+++ Commom_Core/Milestone_2/So_long/src/free.c
@@ -1,29 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   free.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: turocha- <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/14 14:02:33 by turocha-          #+#    #+#             */
-/*   Updated: 2025/10/14 14:02:38 by turocha-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "game.h"
 
-int	quit(t_game *game)
+void	malloc_error(t_game *game, int **matrix, int *row_and_column)
 {
-	if (game && game->mlx && game->window)
-		mlx_destroy_window(game->mlx, game->window);
+	ft_printf("Not enough memory to allocate\n");
+	if (matrix)
+		free_matrix(matrix, row_and_column[0]);
 	free_game(game);
-	if (game && game->mlx)
-	{
-		mlx_destroy_display(game->mlx);
-		free(game->mlx);
-	}
-	free(game);
-	exit(0);
+	exit(1);
 }
 
 void	free_matrix(int **matrix, int rows)
@@ -46,7 +30,7 @@ void	free_map(t_map *map)
 	if (map)
 	{
 		if (map->matrix)
-			free_matrix(map->matrix, map->row_col[0]);
+			free_matrix(map->matrix, map->row_and_column[0]);
 		free(map);
 	}
 }
@@ -73,10 +57,10 @@ void	free_game(t_game *game)
 		mlx_destroy_image(game->mlx, game->floor_img);
 	if (game->mlx && game->collect_img)
 		mlx_destroy_image(game->mlx, game->collect_img);
-	if (game->mlx && game->exit_open)
-		mlx_destroy_image(game->mlx, game->exit_open);
-	if (game->mlx && game->exit_closed)
-		mlx_destroy_image(game->mlx, game->exit_closed);
+	if (game->mlx && game->exit_open_img)
+		mlx_destroy_image(game->mlx, game->exit_open_img);
+	if (game->mlx && game->exit_closed_img)
+		mlx_destroy_image(game->mlx, game->exit_closed_img);
 	if (game->mlx && game->map)
 		free_map(game->map);
 }
