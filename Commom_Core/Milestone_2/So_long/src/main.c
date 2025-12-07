0eaf685ed51e328c85abb9a25226798c781cc937
SlimeWitch1 <tukaemrys@gmail.com>
2025-12-06 22:00:45 -0300

    Update main.c

--- Commom_Core/Milestone_2/So_long/src/main.c
+++ Commom_Core/Milestone_2/So_long/src/main.c
@@ -9,11 +9,16 @@ int	main(int argc, char **argv)
 	matrix = NULL;
 	if (argc != 2)
 	{
-		ft_printf("Invalid Arguments\n");
+		ft_printf("Error\nInvalid Arguments\n");
 		exit(1);
 	}
 	if (check_map(argv[1]) && set_row_and_column(row_and_column, argv[1]))
 		matrix = create_matrix(row_and_column, matrix, argv[1]);
+	else
+	{
+		ft_printf("Error\nMap must be a rectangle\n");
+		return (0);
+	}
 	check_extension(row_and_column, matrix, argv[1]);
 	if (matrix_error(matrix, row_and_column[0], row_and_column[1]))
 	{
